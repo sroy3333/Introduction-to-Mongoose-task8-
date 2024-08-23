b535a8641a02b443602b0f2aab8a29767b10a191
sroy3333 <sukanyaindia2222@gmail.com>
2024-08-24 00:30:35 +0530

    Introduction to Mongoose(task8)

---
+++ app.js
@@ -0,0 +1,46 @@
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const errorController = require('./controllers/error');
+//const User = require('./models/user');
+
+
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// app.use((req, res, next) => {
+//     User.findByPk('66c6dd9e65af5958fd29e907')
+//         .then(user => {
+//             req.user = new User(user.name, user.email, user.cart, user._id);
+//             next();
+//         })
+//         .catch(err => console.log(err));
+// });
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use(errorController.get404);
+
+mongoose
+    .connect(
+        'mongodb+srv://new-user_31:nJ2eDpCCyyEhlkOz@cluster0.dvyo9.mongodb.net/shop?retryWrites=true&w=majority&appName=Cluster0'
+    )
+    .then(result => {
+        app.listen(3000);
+    })
+    .catch(err => {
+        console.log(err);
+    });
